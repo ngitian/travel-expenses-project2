0c2d35ec4586ae17f3d7e8c1b9267c6762bed5e8
ngitian <yitianhuang@cpp.edu>
2022-03-05 15:43:55 -0800

    implement display

--- output.c
+++ output.c
@@ -1,3 +1,15 @@
+#include <stdio.h>
+
+static float dollarConverter(int centsAmount);
+
 void display(int totalExpense, int totalAllowable) {
-    
+    printf("Here are the results of your travel expenses:\n");
+    printf("Total Expense incurred by the businessperson: $%.2f\n", dollarConverter(totalExpense));
+    printf("Total Allowable expenses for the trip: $%.2f\n", dollarConverter(totalAllowable));
+    printf("Excess reimbursed by the businessperson: $%.2f\n", dollarConverter(totalExpense - totalAllowable));
+    printf("Amount saved by the businessperson: $%.2f\n\n", dollarConverter(totalAllowable - totalExpense));
+}
+
+static float dollarConverter(int centsAmount) {
+    return (float) centsAmount / 100.0;
 }
--- output.h
+++ output.h
@@ -1,4 +1,5 @@
 #ifndef OUTPUT_H
 #define OUTPUT_H
 void display(int totalExpense, int totalAllowable);
+static float dollarConverter(int centsAmount);
 #endif
