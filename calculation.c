a39a56e376f5fffd9ca9754d34f397154025f2d7
ngitian <yitianhuang@cpp.edu>
2022-03-05 14:47:30 -0800

    implement skeleton code for calculation

--- calculation.c
+++ calculation.c
@@ -1,1 +1,31 @@
+void calculateAirfare(int airFare, int *totalExpense, int *totalAllowable) {
 
+}
+
+void calculateCarRental(int carRental, int *totalExpense, int *totalAllowable) {
+
+}
+
+void calculateVehicleExpense(int vehicleExpense, int *totalExpense, int *totalAllowable) {
+
+}
+
+void calculateParkingFees(int parkingFees, int numOfDays, int *totalExpense, int *totalAllowable) {
+
+}
+
+void calculateTaxiFees(int taxiFees, int numOfDays, int *totalExpense, int *totalAllowable) {
+
+}
+
+void calculateRegistrationFees(int registrationFees, int *totalExpense, int *totalAllowable) {
+
+}
+
+void calculateHotelExpenses(int hotelExpenses, int numOfDays, int *totalExpense, int *totalAllowable) {
+
+}
+
+void calculateMealExpenses(int mealExpenses, int numOfDays, int  timeDeparture, int timeArrival, int *totalExpense, int *totalAllowable) {
+
+}
