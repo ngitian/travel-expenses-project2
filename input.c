c96c7e1ffd4cccfa8f8ffcda67fd778a61557c20
ngitian <yitianhuang@cpp.edu>
2022-03-05 14:41:36 -0800

    implement skeleton code for input

--- input.c
+++ input.c
@@ -1,1 +1,60 @@
 
+int getNumOfDays() {
+    int result = 0;
+
+    return result;
+}
+
+int getTimeDeparture() {
+    int result = 0;
+
+    return result;
+} 
+
+int getTimeArrival() {
+    int result = 0;
+
+    return result;
+}
+
+int getAirFare() {
+    int result = 0;
+
+    return result;
+}
+
+int getCarRental() {
+    int result = 0;
+
+    return result;
+}
+
+int getVehicleExpense() {
+    int result = 0;
+
+    return result;
+}
+
+int getParkingFee() {
+    int result = 0;
+
+    return result;
+}
+
+int getTaxiFee() {
+    int result = 0;
+
+    return result;
+}
+
+int getRegistrationFee() {
+    int result = 0;
+
+    return result;
+}
+
+int getHotelExpense() {
+    int result = 0;
+
+    return result;
+}
--- input.h
+++ input.h
@@ -1,1 +1,13 @@
-
+#ifndef INPUT_H
+#define INPUT_H
+int getNumOfDays();
+int getTimeDeparture(); 
+int getTimeArrival();
+int getAirFare();
+int getCarRental();
+int getVehicleExpense();
+int getParkingFee();
+int getTaxiFee();
+int getRegistrationFee();
+int getHotelExpense();
+#endif
