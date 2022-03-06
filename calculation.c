ff0a1d9ac5da093444ba856ba6d29d218273ccd2
ngitian <yitianhuang@cpp.edu>
2022-03-05 16:27:59 -0800

    implement few functions

--- calculation.c
+++ calculation.c
@@ -1,17 +1,26 @@
-void calculateAirfare(int airFare, int *totalExpense, int *totalAllowable) {
+const int EXPENSE_PER_MILE = 27;
+const int PARKING_ALLOWABLE_PER_DAY = 600;
+const int TAXI_ALLOWABLE_PER_DAY = 1000;
+const int HOTEL_ALLOWABLE_PER_DAY = 9000;
 
+void calculateAirfare(int airFare, int *totalExpense, int *totalAllowable) {
+    *totalExpense += airFare;
+    *totalAllowable += airFare;
 }
 
 void calculateCarRental(int carRental, int *totalExpense, int *totalAllowable) {
-
+    *totalExpense += carRental;
+    *totalAllowable += carRental;
 }
 
-void calculateVehicleExpense(int vehicleExpense, int *totalExpense, int *totalAllowable) {
-
+void calculateVehicleExpense(int mileDriven, int *totalExpense, int *totalAllowable) {
+    *totalExpense += mileDriven * EXPENSE_PER_MILE;
+    *totalAllowable += mileDriven * EXPENSE_PER_MILE;
 }
 
 void calculateParkingFees(int parkingFees, int numOfDays, int *totalExpense, int *totalAllowable) {
-
+    *totalExpense += parkingFees;
+    *totalAllowable += numOfDays * PARKING_ALLOWABLE_PER_DAY;
 }
 
 void calculateTaxiFees(int taxiFees, int numOfDays, int *totalExpense, int *totalAllowable) {
@@ -19,11 +28,13 @@ void calculateTaxiFees(int taxiFees, int numOfDays, int *totalExpense, int *tota
 }
 
 void calculateRegistrationFees(int registrationFees, int *totalExpense, int *totalAllowable) {
-
+    *totalExpense += registrationFees;
+    *totalAllowable += registrationFees;
 }
 
 void calculateHotelExpenses(int hotelExpenses, int numOfDays, int *totalExpense, int *totalAllowable) {
-
+    *totalExpense += hotelExpenses;
+    *totalAllowable += numOfDays * HOTEL_ALLOWABLE_PER_DAY;
 }
 
 void calculateMealExpenses(int mealExpenses, int numOfDays, int  timeDeparture, int timeArrival, int *totalExpense, int *totalAllowable) {
--- calculation.h
+++ calculation.h
@@ -2,7 +2,7 @@
 #define CALCULATION_H
 void calculateAirfare(int airFare, int *totalExpense, int *totalAllowable);
 void calculateCarRental(int carRental, int *totalExpense, int *totalAllowable);
-void calculateVehicleExpense(int vehicleExpense, int *totalExpense, int *totalAllowable);
+void calculateVehicleExpense(int mileDriven, int *totalExpense, int *totalAllowable);
 void calculateParkingFees(int parkingFees, int numOfDays, int *totalExpense, int *totalAllowable);
 void calculateTaxiFees(int taxiFees, int numOfDays, int *totalExpense, int *totalAllowable);
 void calculateRegistrationFees(int registrationFees, int *totalExpense, int *totalAllowable);
